8a9a1db34b12c17f34034a3e3ded5afe9090da57
Eric Tuchscherer <eric@tuchscherer.me>
2020-07-11 14:41:48 -0700

    Enfore single quotes, autofixing

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import "module-alias/register";
-import express from "express";
-import { info } from "@/services/logging";
+import 'module-alias/register';
+import express from 'express';
+import { info } from '@/services/logging';
 
 const app = express();
 const port = 8080; // default port to listen
@@ -12,8 +12,8 @@ async function startServer() {
   await bootstrap();
 
   // define a route handler for the default home page
-  app.get("/", (req, res) => {
-    res.send("Hello world!");
+  app.get('/', (req, res) => {
+    res.send('Hello world!');
   });
 
   // start the Express server
