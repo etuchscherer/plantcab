ee742a1686e9969e76ecea04002af9a479bb049c
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2020-08-18 22:04:02 -0700

    Adding a svelte front-end

---
+++ svelte-frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+  import { Router, Link, Route } from 'svelte-routing';
+  import Main from './routes/Main.svelte';
+
+  export let url: string = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/reports">Reports</Link>
+    <Link to="/main">Main</Link>
+    <Link to="/maintenance">Maintenance</Link>
+  </nav>
+  <Route path="/main"><Main /></Route>
+</Router>
