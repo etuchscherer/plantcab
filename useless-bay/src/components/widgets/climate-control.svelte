8a4d0f297d414878deeb1fdd617d0b195086518a
Eric Tuchscherer <eric@tuchscherer.me>
2020-12-19 16:30:33 -0800

    climate control adjustments

--- useless-bay/src/components/widgets/climate-control.svelte
+++ useless-bay/src/components/widgets/climate-control.svelte
@@ -4,15 +4,17 @@
     export let pageName: string = '';
 </script>
 
-<IndicatorBorder pageName="{pageName}" >
-    <div slot="main-slot" class="container uppercase">
+<IndicatorBorder pageName="{pageName}" classes="mx-1">
+    <div slot="main-slot" class="container uppercase text-center">
         <p>
-            Set desired max temp
+            Set desired max&nbsp;temp
         </p>
+        <button class="temp-button mb-1">78°</button>
         <hr>
-        <p>
-            Set desired low temp
+        <p class="mt-1">
+            Set desired low&nbsp;temp
         </p>
+        <button class="temp-button">72°</button>
     </div>
 </IndicatorBorder>
 
@@ -20,12 +22,22 @@
     .container {
         max-width: 148px;
         color: rgba(253, 185, 19, 1);
-        font-size: 16px;
+        font-size: 15px;
+        font-family: "NeutraDemi";
     }
 
     hr {
         height: 1px;
         border: none;
         background-color: rgba(253, 185, 19, 1);
+    }
+
+    .temp-button {
+        background-color: #f9f9f9;
+        width: 122px;
+        height: 42px;
+        border-radius: 8px;
+        color: #58595b;
+        font-size: 32px;
     }
 </style>
