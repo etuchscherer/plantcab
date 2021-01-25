9ca27782da445d57a97cbc58716192226216da4d
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2021-01-24 18:13:08 -0800

    lint fixes and removing comments

--- api/src/services/systemManager.test.ts
+++ api/src/services/systemManager.test.ts
@@ -1,5 +1,4 @@
 import SystemManager from '@/services/systemManager';
-import Board from '@/services/board';
 
 let manager: SystemManager;
 
@@ -12,14 +11,4 @@ describe('system manager tests', () => {
   test('can be instansiated', () => {
     expect(manager).toBeInstanceOf(SystemManager);
   });
-
-  // test('sets up the board, when instansiated', () => {
-  //   expect(manager.get('main_board')).toBeInstanceOf(Board);
-  // });
-
-  // test('can debug the cache', () => {
-  //   manager.flush();
-  //   manager.set('foo', 'bar');
-  //   expect(manager.dumpCache()).toStrictEqual({ foo: 'bar' });
-  // });
 });
--- api/src/services/systemManager.ts
+++ api/src/services/systemManager.ts
@@ -6,13 +6,7 @@ export default class SystemManager {
   public board: Board;
 
   constructor() {
-    // super();
     const boardOptions = {}
     this.board = new Board(boardOptions);
-    // this.set('main_board', new Board(boardOptions));
   }
-
-  // dumpCache(): unknown {
-  //   return super.debug();
-  // }
 }
